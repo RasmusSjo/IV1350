1f34d5638b2cbaa8e0dec41f28c61d60b9738c5d
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-04 00:24:48 +0200

    feat: add functionality for printing a receipt

--- src/main/se/kth/iv1350/rassjo/pos/integration/ReceiptPrinter.java
+++ src/main/se/kth/iv1350/rassjo/pos/integration/ReceiptPrinter.java
@@ -2,13 +2,27 @@ package main.se.kth.iv1350.rassjo.pos.integration;
 
 import main.se.kth.iv1350.rassjo.pos.model.Receipt;
 
+/**
+ * The ReceiptPrinter class is responsible for printing receipts to the console.
+ * It takes a {@link Receipt} object and outputs its string representation.
+ */
 public class ReceiptPrinter {
 
+    /**
+     * Creates a new instance of the ReceiptPrinter.
+     * This constructor initializes the ReceiptPrinter, which is responsible
+     * for formatting and printing receipt information.
+     */
     public ReceiptPrinter() {
-
     }
 
+    /**
+     * Prints the string representation of the provided {@code Receipt} object to the console.
+     *
+     * @param receipt the {@code Receipt} object containing sales and payment information to be printed.
+     *                The receipt's {@code toString} method is used for formatting the output.
+     */
     public void printReceipt(Receipt receipt) {
-
+        System.out.println(receipt.toString());
     }
 }
--- src/main/se/kth/iv1350/rassjo/pos/model/Receipt.java
+++ src/main/se/kth/iv1350/rassjo/pos/model/Receipt.java
@@ -1,14 +1,63 @@
 package main.se.kth.iv1350.rassjo.pos.model;
 
 import main.se.kth.iv1350.rassjo.pos.integration.DTOs.SaleDTO;
+import main.se.kth.iv1350.rassjo.pos.integration.DTOs.SaleItemDTO;
+
+import java.time.format.DateTimeFormatter;
 
 public class Receipt {
 
-    public Receipt(CashPayment payment, SaleDTO saleInformation) {
+    private static final int RECEIPT_WIDTH = 80;
+    private static final int ITEM_QUANTITY_PLACEMENT = 50;
+    private static final int SUMMARY_PLACEMENT = 70;
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final String ITEM_LINE_FORMAT =
+            "%-" + ITEM_QUANTITY_PLACEMENT + "s %-" + (SUMMARY_PLACEMENT - ITEM_QUANTITY_PLACEMENT) + "s";
+    private static final String SUMMARY_LINE_FORMAT = "%-" + SUMMARY_PLACEMENT + "s";
 
+    private final CashPayment payment;
+    private final SaleDTO saleInformation;
+
+    public Receipt(CashPayment payment, SaleDTO saleInformation) {
+        this.payment = payment;
+        this.saleInformation = saleInformation;
     }
 
+    @Override
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        String beginReceipt = " Begin receipt ";
+        int topSidePadding = (RECEIPT_WIDTH - beginReceipt.length()) / 2;
+
+        // Header
+        sb.append("-".repeat(topSidePadding)).append(beginReceipt).append("-".repeat(topSidePadding));
+
+        // Time of sale
+        sb.append("Time of Sale: ").append(saleInformation.startTime().format(TIME_FORMAT)).append("\n\n");
+
+        // Items
+        for (SaleItemDTO item : saleInformation.items()) {
+            Amount totalItemCost = new Amount(item.quantity() * item.finalPrice().amount());
+            String quantityAndPrice = item.quantity() + " x " + item.netPrice();
+            sb.append(String.format(ITEM_LINE_FORMAT, item.name(), quantityAndPrice));
+            sb.append(totalItemCost).append(" SEK\n");
+        }
+        sb.append("\n");
+
+        // Totals
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Total:")).append(saleInformation.totalCost()).append(" SEK\n");
+        sb.append("VAT: ").append(saleInformation.totalVat()).append(" SEK\n\n");
+
+        // Payment
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Payment:")).append(payment.getPaidAmount()).append(" SEK\n");
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Change:")).append(payment.getChange()).append(" SEK\n");
+
+        // Footer
+        String endReceipt = " End receipt ";
+        int bottomSidePadding = (RECEIPT_WIDTH - endReceipt.length()) / 2;
+        sb.append("-".repeat(bottomSidePadding)).append(" End receipt ").append("-".repeat(bottomSidePadding));
+
+        return sb.toString();
     }
 }
