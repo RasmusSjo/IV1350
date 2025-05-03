153806ffb20d60e157cde61c9589fd360fffdf45
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-04 00:24:26 +0200

    refactor: change variable name

--- src/main/se/kth/iv1350/rassjo/pos/integration/DTOs/SaleItemDTO.java
+++ src/main/se/kth/iv1350/rassjo/pos/integration/DTOs/SaleItemDTO.java
@@ -7,11 +7,11 @@ package main.se.kth.iv1350.rassjo.pos.integration.DTOs;
  * @param id          the unique identifier of the item in the inventory system.
  * @param name        the name of the item.
  * @param description a brief description of the item.
- * @param unitPrice   the price per unit before VAT and discounts.
+ * @param netPrice   the price per unit before VAT and discounts.
  * @param vatRate     the VAT percentage applied to the item.
  * @param finalPrice  the price per unit after applying VAT and any potential discounts.
  * @param quantity    the number of items of this type that in the sale.
  */
-public record SaleItemDTO(ItemIdentifierDTO id, String name, String description, AmountDTO unitPrice,
+public record SaleItemDTO(ItemIdentifierDTO id, String name, String description, AmountDTO netPrice,
                           PercentageDTO vatRate, AmountDTO finalPrice, int quantity) {
 }
