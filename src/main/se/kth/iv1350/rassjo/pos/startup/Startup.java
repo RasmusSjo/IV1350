ca2274254398831e23be47c74e3a1a2635e35c44
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-03 20:56:08 +0200

    fix: made the main method static

--- src/main/se/kth/iv1350/rassjo/pos/startup/Startup.java
+++ src/main/se/kth/iv1350/rassjo/pos/startup/Startup.java
@@ -1,12 +1,13 @@
 package main.se.kth.iv1350.rassjo.pos.startup;
 
 import main.se.kth.iv1350.rassjo.pos.controller.SaleController;
+import main.se.kth.iv1350.rassjo.pos.integration.DTOs.AmountDTO;
 import main.se.kth.iv1350.rassjo.pos.integration.HandlerFactory;
 import main.se.kth.iv1350.rassjo.pos.view.View;
 
 public class Startup {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         HandlerFactory handlerFactory = new HandlerFactory();
         SaleController saleController = new SaleController(handlerFactory);
         View view = new View(saleController);
