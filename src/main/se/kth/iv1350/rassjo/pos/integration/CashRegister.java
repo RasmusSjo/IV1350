b29adf2dbde33b5c01e014a22313f464734b2d28
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-04 00:25:10 +0200

    feat: add cash handling functionality

--- src/main/se/kth/iv1350/rassjo/pos/integration/CashRegister.java
+++ src/main/se/kth/iv1350/rassjo/pos/integration/CashRegister.java
@@ -3,19 +3,40 @@ package main.se.kth.iv1350.rassjo.pos.integration;
 import main.se.kth.iv1350.rassjo.pos.model.Amount;
 import main.se.kth.iv1350.rassjo.pos.model.CashPayment;
 
+/**
+ * Represents a cash register used to track transactions
+ * and maintain a running balance. It allows adding payments
+ * and dispensing change.
+ */
 public class CashRegister {
 
-    private Amount currentBalance;
+    private static final double STARTUP_BALANCE = 1000;
+    private final Amount currentBalance;
 
+    /**
+     * Initializes a new instance of the CashRegister class with a predefined startup balance.
+     */
     public CashRegister() {
-
+        currentBalance = new Amount(STARTUP_BALANCE);
     }
 
+    /**
+     * Adds a cash payment to the cash register and updates the current balance
+     * with the paid amount.
+     *
+     * @param payment the {@code CashPayment} containing the paid amount to be added.
+     */
     public void addPayment(CashPayment payment) {
-
+        currentBalance.increaseAmountBySum(payment.getPaidAmount());
     }
 
-    public void dispenseChange(int change) {
-
+    /**
+     * Dispenses the specified monetary amount as change by reducing the current balance
+     * of the cash register by the given amount.
+     *
+     * @param amount the {@code Amount} representing the monetary value to be dispensed as change.
+     */
+    public void dispenseChange(Amount amount) {
+        currentBalance.decreaseAmountBySum(amount);
     }
 }
