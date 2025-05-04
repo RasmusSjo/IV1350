1f2a995f18d6bfe97125f390604d577df9d9b82f
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-04 02:59:05 +0200

    refactor(receipt): adjust formatting and alignment for a better receipt layout

--- src/main/se/kth/iv1350/rassjo/pos/model/Receipt.java
+++ src/main/se/kth/iv1350/rassjo/pos/model/Receipt.java
@@ -7,13 +7,16 @@ import java.time.format.DateTimeFormatter;
 
 public class Receipt {
 
-    private static final int RECEIPT_WIDTH = 80;
-    private static final int ITEM_QUANTITY_PLACEMENT = 50;
-    private static final int SUMMARY_PLACEMENT = 70;
+    private static final int RECEIPT_WIDTH = 56;
+    private static final int ITEM_QUANTITY_PLACEMENT = 28;
+    private static final int SUMMARY_PLACEMENT = 40;
     private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final String ITEM_LINE_FORMAT =
-            "%-" + ITEM_QUANTITY_PLACEMENT + "s %-" + (SUMMARY_PLACEMENT - ITEM_QUANTITY_PLACEMENT) + "s";
-    private static final String SUMMARY_LINE_FORMAT = "%-" + SUMMARY_PLACEMENT + "s";
+            "%-" + ITEM_QUANTITY_PLACEMENT + "s " +
+            "%-" + (SUMMARY_PLACEMENT - ITEM_QUANTITY_PLACEMENT) + "s " +
+            "%" + (RECEIPT_WIDTH - SUMMARY_PLACEMENT - 2) + "s\n";
+    private static final String SUMMARY_LINE_FORMAT =
+            "%-" + SUMMARY_PLACEMENT + "s %" + (RECEIPT_WIDTH - SUMMARY_PLACEMENT - 2) + "s\n";
 
     private final CashPayment payment;
     private final SaleDTO saleInformation;
@@ -28,10 +31,10 @@ public class Receipt {
         StringBuilder sb = new StringBuilder();
 
         String beginReceipt = " Begin receipt ";
-        int topSidePadding = (RECEIPT_WIDTH - beginReceipt.length()) / 2;
+        int topSidePadding = (RECEIPT_WIDTH - beginReceipt.length() + 1) / 2;
 
         // Header
-        sb.append("-".repeat(topSidePadding)).append(beginReceipt).append("-".repeat(topSidePadding));
+        sb.append("-".repeat(topSidePadding)).append(beginReceipt).append("-".repeat(topSidePadding)).append("\n");
 
         // Time of sale
         sb.append("Time of Sale: ").append(saleInformation.startTime().format(TIME_FORMAT)).append("\n\n");
@@ -40,22 +43,21 @@ public class Receipt {
         for (SaleItemDTO item : saleInformation.items()) {
             Amount totalItemCost = new Amount(item.quantity() * item.finalPrice().amount());
             String quantityAndPrice = item.quantity() + " x " + item.netPrice();
-            sb.append(String.format(ITEM_LINE_FORMAT, item.name(), quantityAndPrice));
-            sb.append(totalItemCost).append(" SEK\n");
+            sb.append(String.format(ITEM_LINE_FORMAT, item.name(), quantityAndPrice, totalItemCost + " SEK"));
         }
         sb.append("\n");
 
         // Totals
-        sb.append(String.format(SUMMARY_LINE_FORMAT, "Total:")).append(saleInformation.totalCost()).append(" SEK\n");
-        sb.append("VAT: ").append(saleInformation.totalVat()).append(" SEK\n\n");
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Total:", saleInformation.totalCost() + " SEK"));
+        sb.append("VAT: ").append(saleInformation.totalVat()).append("\n\n");
 
         // Payment
-        sb.append(String.format(SUMMARY_LINE_FORMAT, "Payment:")).append(payment.getPaidAmount()).append(" SEK\n");
-        sb.append(String.format(SUMMARY_LINE_FORMAT, "Change:")).append(payment.getChange()).append(" SEK\n");
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Payment:", payment.getPaidAmount() + " SEK"));
+        sb.append(String.format(SUMMARY_LINE_FORMAT, "Change:", payment.getChange() + " SEK"));
 
         // Footer
         String endReceipt = " End receipt ";
-        int bottomSidePadding = (RECEIPT_WIDTH - endReceipt.length()) / 2;
+        int bottomSidePadding = (RECEIPT_WIDTH - endReceipt.length() + 1) / 2;
         sb.append("-".repeat(bottomSidePadding)).append(" End receipt ").append("-".repeat(bottomSidePadding));
 
         return sb.toString();
