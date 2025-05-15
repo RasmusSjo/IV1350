8661144af23623b92d4629bd07382e0b5e47718a
RasmusSjo <rasmus.sjoberg15@gmail.com>
2025-05-15 21:57:09 +0200

    docs: add Javadoc for DiscountHandler

--- src/main/java/se/kth/iv1350/rassjo/pos/integration/DiscountHandler.java
+++ src/main/java/se/kth/iv1350/rassjo/pos/integration/DiscountHandler.java
@@ -3,13 +3,28 @@ package se.kth.iv1350.rassjo.pos.integration;
 import se.kth.iv1350.rassjo.pos.integration.DTOs.DiscountDTO;
 import se.kth.iv1350.rassjo.pos.integration.DTOs.DiscountRequestDTO;
 
+/**
+ * The {@code DiscountHandler} class is responsible for managing discounts requests
+ * in the POS system by interacting with an external discount database.
+ * <p/>
+ * This class is just for simulation and doesn't contain any functionality.
+ */
 public class DiscountHandler {
 
+    /**
+     * Creates a new instance of a {@link DiscountHandler}.
+     */
     DiscountHandler() {
-
     }
 
     public DiscountDTO getDiscount(DiscountRequestDTO discountRequest) {
         return null;
+    /**
+     * Retrieves applicable discounts for a sale based on the provided discount request.
+     *
+     * @param discountRequest the {@link DiscountRequestDTO} containing information
+     *                        about the customer and sale.
+     * @return a {@link DiscountDTO} containing the calculated discount information.
+     */
     }
 }
